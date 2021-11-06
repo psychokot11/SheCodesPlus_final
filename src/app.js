73734b4de53c2a9f648228847706a61ad52e36a4
psychokot11 <z.stawarska@gmail.com>
2021-11-06 21:49:09 +0100

    Added <form> element and eventListener for search engine
    IMPORTANT: for the search form to be working ("submit"), the whole form element must be selected not only the button

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 function displayTempAndDate(response) {
+  //temperature and conditions
   let tempElement = document.querySelector("#temperature");
   tempElement.innerHTML = Math.round(response.data.main.temp);
   let conditions = document.querySelector("#main-conditions");
@@ -8,10 +9,18 @@ function displayTempAndDate(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  //date and time
   let dateElement = document.querySelector("#current-day");
   let date = new Date();
   let hour = date.getHours();
   let minute = date.getMinutes();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
   let weekDay = [
     "Sunday",
     "Monday",
@@ -23,16 +32,37 @@ function displayTempAndDate(response) {
   ];
   let day = date.getDate();
   dateElement.innerHTML = `${weekDay[day]} ${hour}:${minute}`;
+
+  // celcius to farenheit
+  let celciusTemp = document.querySelector("#celcius");
+  let farenheitTemp = document.querySelector("#farenheit");
+
+  function changeToCelcius(event) {
+    event.preventDefault();
+    tempElement.innerHTML = Math.round(response.data.main.temp);
+  }
+
+  function changeToFarenheit(event) {
+    event.preventDefault();
+    tempElement.innerHTML = Math.round((response.data.main.temp * 9) / 5 + 32);
+  }
+
+  celciusTemp.addEventListener("click", changeToCelcius);
+  farenheitTemp.addEventListener("click", changeToFarenheit);
 }
 
 let weatherApiKey = "5f499f0d563e2b69490e35e28cf5fd01";
 let city = "Paris";
 let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApiKey}&units=metric`;
-console.log(weatherUrl);
-
 axios.get(weatherUrl).then(displayTempAndDate);
 
-//let date = Date.
+let searchButtonElem = document.querySelector("#form");
 
-//let dateElement = document.querySelector("#current-day");
-//dateElement.innerHTML = day[date]
+function searchCity(event) {
+  event.preventDefault();
+  let searchFieldInput = document.querySelector("#search-field");
+  let searchedCity = document.querySelector("#searched-city");
+  searchedCity.innerHTML = `${searchFieldInput.value}`;
+  console.log(searchFieldInput.value);
+}
+searchButtonElem.addEventListener("submit", searchCity);
