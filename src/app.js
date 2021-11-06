e2ea60d7a73120b7853c91a6047eccdd2591c2b5
psychokot11 <z.stawarska@gmail.com>
2021-11-06 18:43:31 +0100

    Finished writing displayTempAndDate function
    Added date and time

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-function displayTemp(response) {
+function displayTempAndDate(response) {
   let tempElement = document.querySelector("#temperature");
   tempElement.innerHTML = Math.round(response.data.main.temp);
   let conditions = document.querySelector("#main-conditions");
@@ -8,6 +8,21 @@ function displayTemp(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#current-day");
+  let date = new Date();
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let weekDay = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = date.getDate();
+  dateElement.innerHTML = `${weekDay[day]} ${hour}:${minute}`;
 }
 
 let weatherApiKey = "5f499f0d563e2b69490e35e28cf5fd01";
@@ -15,17 +30,9 @@ let city = "Paris";
 let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApiKey}&units=metric`;
 console.log(weatherUrl);
 
-axios.get(weatherUrl).then(displayTemp);
+axios.get(weatherUrl).then(displayTempAndDate);
 
 //let date = Date.
-//let day = [
-//"Sunday",
-//"Monday",
-//"Tuesday",
-//"Wednesday",
-//"Thursday",
-//"Friday",
-//"Saturday",
-//];
+
 //let dateElement = document.querySelector("#current-day");
 //dateElement.innerHTML = day[date]
